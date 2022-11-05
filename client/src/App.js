cc08bd29da92d05dacf6e699bff2bc73bf0711c3
Surojeet Ghosh <surojeetghosh89@gmail.com>
2022-11-05 10:24:29 +0530

    Form creation

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,20 @@
 import React from "react";
 import NavBar from "./components/NavBar";
 import Carousel from "./components/Carousel";
-import Footer from "./components/Footer"
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Footer from "./components/Footer";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 function App() {
   return (
     <div>
       <NavBar />
-      <hr className="seperator rounded"/>
-      {/* <Carousel /> */}
+      <Routes>
+        <Route path="/" element={<Carousel />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+      </Routes>    
       <Footer />
     </div>
   );
